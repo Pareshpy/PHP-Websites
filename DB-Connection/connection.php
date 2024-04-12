fc3f84396ae4c5e3b49cfdcc4d46bc05935daad2
Paresh <pareshnathnandi1234@gmail.com>
2024-04-12 20:34:53 +0530

    Connection.php and index.php file added

---
+++ DB-Connection/connection.php
@@ -0,0 +1,11 @@
+<?php
+    $server_name = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "leafvillage";
+    $conn = new mysqli($server_name , $username , $password , $db_name);
+    if($conn -> connect_error){
+        die("Database connection failed".$conn ->connect_error);
+    }
+    echo "Database connected";
+?>
---
+++ DB-Connection/index.php
@@ -0,0 +1,14 @@
+<?php
+    include("connection.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h3>hello DB</h3>
+</body>
+</html>

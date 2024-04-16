23d465139d1266fcd470846b8303c93247a7da2d
Paresh <pareshnathnandi1234@gmail.com>
2024-04-16 20:55:49 +0530

    adding css for welcome.php

--- DB-Connection/welcome.php
+++ DB-Connection/welcome.php
@@ -1,11 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: "poppins", sans-serif;
+        }
+
+        body {
+            min-height: 100vh;
+            /* background-image: url('bg.png'); */
+            background-image: linear-gradient(94deg, #7950f2 5%, #4dabf7 95%);
+            background-size: cover;
+            background-position: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }   
+        h2{
+            color: whitesmoke;
+            font-size: 45px;
+        }
+    </style>
 </head>
+
 <body>
-    <h2>Login Successful </h2>
+    <h2>Login Successful</h2>
+    </div>
 </body>
+
 </html>
